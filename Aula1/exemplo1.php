bde3cba7f3e1ea0c45ac08c6b8a8f02a25ab1071
Giovana de Moura <giovana.m.souza7@aluno.senai.br>
2025-10-10 16:31:38 -0300

    feito
    4 exemplos de php realizados

---
+++ Aula1/exemplo1.php
@@ -0,0 +1,18 @@
+<?php
+  //Geração de texto (string)
+  echo "Chama Sesi-Senai!" ; 
+
+/*
+Geração de texto estruturado 
+Com tags e atributos 
+*/
+// Aspas simples dentro das aspas dupla
+echo "<h2>Gerando <span class='destaque'>HTML</span> atraves do PHP.</h2>
+";
+
+// Aspas simples dentro das aspas dupla
+echo "<h2>Gerando <span class=\"destaque\">HTML</span> atraves do PHP.</h2>
+";
+
+
+?>
